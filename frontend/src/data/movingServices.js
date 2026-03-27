2fb62950523e4ca1fba2eba24f0a63a8b9b97739
masxkip <jesufemiketiku@gmail.com>
2026-03-27 05:23:29 -0700

    new

--- frontend/src/data/movingServices.js
+++ frontend/src/data/movingServices.js
@@ -1,52 +1,24 @@
+// File: src/data/movingServices.js
+import m1 from "../assets/m/m1.png";
+import m4 from "../assets/m/m4.png";
+import m5 from "../assets/m/m5.png";
+import m7 from "../assets/m/m7.png";
+import m8 from "../assets/m/m8.png";
+import m6 from "../assets/m/m6.png";
+
 const movingServices = [
-  {
-    id: 1,
-    title: "24/7/365 Moving",
-    description:
-      "The only moving company in NYC that offers any day and time moving services. No matter how big or small your move is we will move it.",
-    image: "../../src/assets/m/m1.png",
-    link: "/services/24-7-moving",
-  },
-  {
-    id: 2,
-    title: "Local Residential Moving Services",
-    description:
-      "We specialize in local residential NYC and New York State moves. We can move you across all of New York City's locations.",
-    image: "../../src/assets/m/m8.png",
-    link: "/services/local-residential-moving",
-  },
-  {
-    id: 3,
-    title: "Long Distance Moving Services",
-    description:
-      "From NYC to Miami, California and Seattle we can move you to any state in the USA with reliable long distance service.",
-    image: "../../src/assets/m/m4.png",
-    link: "/services/long-distance-moving",
-  },
-  {
-    id: 4,
-    title: "Office and Commercial Moves",
-    description:
-      "We can move your office overnight, on weekends or holidays to minimize downtime so your business stays running.",
-    image: "../../src/assets/m/m5.png",
-    link: "/services/office-commercial-moving",
-  },
-  {
-    id: 5,
-    title: "Small Moves",
-    description:
-      "Need to move a few items or moving out of a studio with minimal furniture? Our small move service makes it easy.",
-    image: "../../src/assets/m/m6.png",
-    link: "/services/small-moves",
-  },
-  {
-    id: 6,
-    title: "Last Minute Moves",
-    description:
-      "Need to move urgently? Our last minute moving service will save the day and get you settled quickly.",
-    image: "../../src/assets/m/m7.png",
-    link: "/services/last-minute-moves",
-  },
+  { id: 1, title: "24/7/365 Moving", description:
+      "The only moving company in NYC that offers any day and time moving services. No matter how big or small your move is we will move it.", image: m1, link: "/services/24-7-moving" },
+  { id: 2, title: "Local Residential Moving Services", description:
+      "We specialize in local residential NYC and New York State moves. We can move you across all of New York City's locations.", image: m8, link: "/services/local-residential-moving" },
+  { id: 3, title: "Long Distance Moving Services", description:
+      "From NYC to Miami, California and Seattle we can move you to any state in the USA with reliable long distance service.", image: m4, link: "/services/long-distance-moving" },
+  { id: 4, title: "Office and Commercial Moves", description:
+      "We can move your office overnight, on weekends or holidays to minimize downtime so your business stays running.", image: m5, link: "/services/office-commercial-moving" },
+  { id: 5, title: "Small Moves", description:
+      "Need to move a few items or moving out of a studio with minimal furniture? Our small move service makes it easy.", image: m6, link: "/services/small-moves" },
+  { id: 6, title: "Last Minute Moves", description:
+      "Need to move urgently? Our last minute moving service will save the day and get you settled quickly.", image: m7, link: "/services/last-minute-moves" },
 ];
 
 export default movingServices;
