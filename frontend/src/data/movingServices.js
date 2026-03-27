3b8b7734475b6d18212aa00374e7d80326e09d7a
masxkip <jesufemiketiku@gmail.com>
2026-03-27 05:05:44 -0700

    new

--- frontend/src/data/movingServices.js
+++ frontend/src/data/movingServices.js
@@ -4,7 +4,7 @@ const movingServices = [
     title: "24/7/365 Moving",
     description:
       "The only moving company in NYC that offers any day and time moving services. No matter how big or small your move is we will move it.",
-    image: "../../src/assets/m/m1.png",
+    image: "../assets/m/m1.png",
     link: "/services/24-7-moving",
   },
   {
@@ -12,7 +12,7 @@ const movingServices = [
     title: "Local Residential Moving Services",
     description:
       "We specialize in local residential NYC and New York State moves. We can move you across all of New York City's locations.",
-    image: "../../src/assets/m/m8.png",
+    image: "../assets/m/m8.png",
     link: "/services/local-residential-moving",
   },
   {
@@ -20,7 +20,7 @@ const movingServices = [
     title: "Long Distance Moving Services",
     description:
       "From NYC to Miami, California and Seattle we can move you to any state in the USA with reliable long distance service.",
-    image: "../../src/assets/m/m4.png",
+    image: "../assets/m/m4.png",
     link: "/services/long-distance-moving",
   },
   {
@@ -28,7 +28,7 @@ const movingServices = [
     title: "Office and Commercial Moves",
     description:
       "We can move your office overnight, on weekends or holidays to minimize downtime so your business stays running.",
-    image: "../../src/assets/m/m5.png",
+    image: "../assets/m/m5.png",
     link: "/services/office-commercial-moving",
   },
   {
@@ -36,7 +36,7 @@ const movingServices = [
     title: "Small Moves",
     description:
       "Need to move a few items or moving out of a studio with minimal furniture? Our small move service makes it easy.",
-    image: "../../src/assets/m/m6.png",
+    image: "../assets/m/m6.png",
     link: "/services/small-moves",
   },
   {
@@ -44,7 +44,7 @@ const movingServices = [
     title: "Last Minute Moves",
     description:
       "Need to move urgently? Our last minute moving service will save the day and get you settled quickly.",
-    image: "../../src/assets/m/m7.png",
+    image: "../assets/m/m7.png",
     link: "/services/last-minute-moves",
   },
 ];
